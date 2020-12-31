f2a242bd13c9670f7ee68730550a0aed5dba035e
Prueba <a_herrerajol@globalhitss.com>
2020-12-31 11:12:21 -0600

    Dinamica

--- src/test/java/DynamicTests.java
+++ src/test/java/DynamicTests.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class DynamicTests {
+import base.BaseTests;
+import com.applitools.eyes.MatchLevel;
+import org.junit.Test;
+
+public class DynamicTests extends BaseTests {
+
+    @Test
+    public void testDynamicContent(){
+        driver.get(System.getProperty("site.dynamic.url"));
+        eyesManager.getEyes().setMatchLevel(MatchLevel.LAYOUT);
+        eyesManager.validateWindow();
+
+    }
 }
